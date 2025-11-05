33c43e856e13950230328b2838791b9bb3a8bdf6
MATHIEU HAMMEL <mtahammel@gmail.com>
2025-11-05 17:52:06 +0100

    initial commit

---
+++ client/src/components/ExperiencesCards.tsx
@@ -0,0 +1,34 @@
+import "./ExperiencesCards.css";
+
+interface ExperiencesCardsProps {
+  name: string;
+  image: string;
+  continent: string;
+  average_price: string;
+  tourist_season: string;
+}
+
+const DestinationCard: React.FC<ExperiencesCardsProps> = ({
+  name,
+  image,
+  continent,
+  average_price,
+  tourist_season,
+}) => {
+  return (
+    <div className="destination-card">
+      <img src={image} alt={name} className="destination-img" />
+
+      <div className="destination-banner">
+        <h3 className="destination-name">{name}</h3>
+        <div className="destination-infos">
+          <p>{continent}</p>
+          <p>{average_price} € (avg)</p>
+          <p>Best season: {tourist_season}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationCard;
