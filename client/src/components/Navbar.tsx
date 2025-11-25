a9a0589800730deac849922677597142bbf99f28
THBD7 <thibaudmichon7@gmail.com>
2025-11-25 11:31:58 +0100

    hiding footer and navbar if Inspiration landscape on mobile

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { type FC, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../assets/images/AurumHorizonsLogoTransparent.png";
 import UserIcon from "../assets/images/user-icon-black.png";
@@ -10,63 +10,69 @@ const Navbar: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
 
+  const location = useLocation();
+
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  const isTargetPage = location.pathname === "/inspiration";
 
   return (
     <>
-      <GlassSurface height={80} opacity={0.05}>
-        <nav className="navbar-content">
-          <div className="navbar__logo">
-            <Link to="/">
-              <img src={logo} alt="Aurum Horizons logo" />
-            </Link>
-          </div>
+      <div className={isTargetPage ? "hide-landscape" : ""}>
+        <GlassSurface height={80} opacity={0.05}>
+          <nav className="navbar-content">
+            <div className="navbar__logo">
+              <Link to="/">
+                <img src={logo} alt="Aurum Horizons logo" />
+              </Link>
+            </div>
 
-          <button
-            type="button"
-            className={`navbar__toggle ${isMenuOpen ? "open" : ""}`}
-            onClick={toggleMenu}
-            aria-label="Toggle navigation"
-          >
-            <span />
-            <span />
-            <span />
-          </button>
-          <div
-            className={`navbar__links-container ${isMenuOpen ? "open" : ""}`}
-          >
-            <ul className="navbar__links navbar__links--left">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/inspiration">Inspiration</Link>
-              </li>
-              <li>
-                <a href="/see-more">Experiences</a>
-              </li>
-            </ul>
+            <button
+              type="button"
+              className={`navbar__toggle ${isMenuOpen ? "open" : ""}`}
+              onClick={toggleMenu}
+              aria-label="Toggle navigation"
+            >
+              <span />
+              <span />
+              <span />
+            </button>
+            <div
+              className={`navbar__links-container ${isMenuOpen ? "open" : ""}`}
+            >
+              <ul className="navbar__links navbar__links--left">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/inspiration">Inspiration</Link>
+                </li>
+                <li>
+                  <a href="/see-more">Experiences</a>
+                </li>
+              </ul>
 
-            <ul className="navbar__links navbar__links--right">
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
-                <a href="/about">About us</a>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  className="navbar-user-btn"
-                  onClick={() => setShowLoginModal(true)}
-                >
-                  <img src={UserIcon} alt="User Icon" />
-                </button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </GlassSurface>
+              <ul className="navbar__links navbar__links--right">
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                  <a href="/about">About us</a>
+                </li>
+                <li>
+                  <button
+                    type="button"
+                    className="navbar-user-btn"
+                    onClick={() => setShowLoginModal(true)}
+                  >
+                    <img src={UserIcon} alt="User Icon" />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </GlassSurface>
+      </div>
 
       {showLoginModal && <Login onClose={() => setShowLoginModal(false)} />}
     </>
